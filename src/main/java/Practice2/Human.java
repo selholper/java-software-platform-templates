efbd76550586c8599a6c6103acf5cd8692769478
Ivan <selholper@gmail.com>
2024-02-21 12:53:34 +0300

    Done Practice2

---
+++ src/main/java/Practice2/Human.java
@@ -0,0 +1,23 @@
+package Practice2;
+
+import java.time.LocalDate;
+
+public record Human(
+        int age,
+        String firstName,
+        String lastName,
+        LocalDate birthDate,
+        int weight
+) {
+    @Override
+    public String toString() {
+        return String.format(
+            "Human = {age: %d, firstName: %s, lastName: %s, birthDate: %s, weight: %d}",
+            age(),
+            firstName(),
+            lastName(),
+            birthDate().toString(),
+            weight()
+        );
+    }
+}
